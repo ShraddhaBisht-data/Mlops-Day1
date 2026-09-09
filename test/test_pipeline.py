d26e52f5c64631fe9bd279105f6cf00064ffad3e
Shraddha Bisht <shraddhabisht2112@gmail.com>
2026-09-09 19:45:54 +0530

    fix file paths

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -5,7 +5,7 @@ import pandas as pd
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def test_data_schema():
-    data_path = os.path.join(BASE_DIR, "data", "data.csv")
+    data_path = os.path.join(BASE_DIR, "Data", "data.csv")
     assert os.path.exists(data_path), "Data file data.csv not found!"
     df = pd.read_csv(data_path)
     expected_cols = {"TV", "Radio", "Newspaper", "Sales"}
@@ -13,7 +13,7 @@ def test_data_schema():
     assert df[["TV", "Radio", "Newspaper"]].isnull().sum().sum() == 0, "Null values found in features"
 
 def test_champion_model_loading_and_prediction():
-    model_path = os.path.join(BASE_DIR, "models", "champion_model.pkl")
+    model_path = os.path.join(BASE_DIR, "Models", "champion_model.pkl")
     assert os.path.exists(model_path), f"Champion model not found at {model_path}"
 
     model = joblib.load(model_path)
